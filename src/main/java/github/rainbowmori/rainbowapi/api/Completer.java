b121d256107013202cd0458ccb716605cb9378cd
rainbowmori <moriyoshi12345@outlook.jp>
2022-12-16 22:53:32 +0900

    I made it easy.

---
+++ src/main/java/github/rainbowmori/rainbowapi/api/Completer.java
@@ -0,0 +1,24 @@
+package github.rainbowmori.rainbowapi.api;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class Completer implements TabCompleter {
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender,
+                                               @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        int length = args.length;
+        List<String> tab = returned(sender, command, label, args);
+        return args[args.length - 1].isEmpty() ? tab : tab.stream().filter(s -> s.toLowerCase().
+                startsWith(args[length - 1].toLowerCase())).collect(Collectors.toList());
+    }
+
+    public abstract List<String> returned(@NotNull CommandSender sender,
+                                          @NotNull Command command, @NotNull String label, @NotNull String[] args);
+}
