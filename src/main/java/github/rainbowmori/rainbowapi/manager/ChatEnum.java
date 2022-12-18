9f3f0a29b141d9420239971af13443486c627f00
rainbowmori <moriyoshi12345@outlook.jp>
2022-12-18 16:47:02 +0900

    add chat enum

--- src/main/java/github/rainbowmori/rainbowapi/manager/ChatEnum.java
+++ src/main/java/github/rainbowmori/rainbowapi/manager/ChatEnum.java
@@ -1,6 +1,9 @@
 package github.rainbowmori.rainbowapi.manager;
 
 public enum ChatEnum {
+    NOT_INTEGER("<red>数字ではありません"),
+    NOT_ENOUGH_WRITE("<red>記入が足りません"),
+    NOT_HAS_ITEM("<red>アイテムを持ってください"),
     NOT_PLAYER("<red>プレイヤーではありません");
 
     public final String text;
