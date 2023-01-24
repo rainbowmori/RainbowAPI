b1752d2bbe85161c0b43ac148b9b93dfa3668e6d
rainbowmori <moriyoshi12345@outlook.jp>
2023-01-24 21:59:00 +0900

    commit

--- src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
+++ src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
@@ -1,5 +1,6 @@
 package github.rainbowmori.rainbowapi.object.playerinput;
 
+import github.rainbowmori.rainbowapi.RainbowAPI;
 import org.bukkit.conversations.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -7,12 +8,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class PlayerInput implements ConversationAbandonedListener {
 
+    private final Plugin plugin;
+
     private final ConversationFactory factory;
     public PlayerInput(Plugin plugin, String prefix, boolean cancelable, Prompt prompt) {
-        var factory = new ConversationFactory(plugin).withPrefix(context -> prefix).withFirstPrompt(prompt);
+        var factory = new ConversationFactory(plugin).withPrefix(context -> prefix + " ").withFirstPrompt(prompt).
+                withLocalEcho(false);
         if(cancelable) factory.withEscapeSequence("cancel");
         factory.addConversationAbandonedListener(this).thatExcludesNonPlayersWithMessage("プレイヤーしか入力できません");
         this.factory = factory;
+        this.plugin = plugin;
     }
 
     public PlayerInput(Plugin plugin, String prefix, boolean cancelable, Prompt prompt,int timeout) {
@@ -21,11 +26,20 @@ public class PlayerInput implements ConversationAbandonedListener {
     }
 
     public void build(Player player) {
-        factory.buildConversation(player).begin();
+        if(isInputted(plugin,player)) throw new RuntimeException("入力中なのにまた入力のメゾットを使用しています player = " + player.getName());
+        else factory.buildConversation(player).begin();
     }
 
     @Override
     public void conversationAbandoned(@NotNull ConversationAbandonedEvent abandonedEvent) {
         if(!abandonedEvent.gracefulExit()) abandonedEvent.getContext().getForWhom().sendRawMessage("入力をキャンセルしました");
+    }
+
+    public static boolean isInputted(Plugin plugin,Player player) {
+        if (player.isConversing()) {
+            RainbowAPI.apis.get(plugin).mcUtil.send(player, "<red>現在入力中です");
+            return true;
+        }
+        return false;
     }
 }
