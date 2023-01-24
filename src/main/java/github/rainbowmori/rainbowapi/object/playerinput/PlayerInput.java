9f13a74e4b488c3f79fa7dbfa13fb1ad4fa7c36b
rainbowmori <moriyoshi12345@outlook.jp>
2023-01-24 16:04:35 +0900

    commit

--- src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
+++ src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
@@ -1,6 +1,7 @@
 package github.rainbowmori.rainbowapi.object.playerinput;
 
 import org.bukkit.conversations.*;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +18,10 @@ public class PlayerInput implements ConversationAbandonedListener {
     public PlayerInput(Plugin plugin, String prefix, boolean cancelable, Prompt prompt,int timeout) {
         this(plugin, prefix, cancelable, prompt);
         this.factory.withTimeout(timeout);
+    }
+
+    public void build(Player player) {
+        factory.buildConversation(player).begin();
     }
 
     @Override
