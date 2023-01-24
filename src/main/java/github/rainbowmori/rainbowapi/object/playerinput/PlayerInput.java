7d5b4a8d6c0741c0120e96ed4a9d208674014f9d
rainbowmori <moriyoshi12345@outlook.jp>
2023-01-24 16:04:35 +0900

    commit

--- src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
+++ src/main/java/github/rainbowmori/rainbowapi/object/playerinput/PlayerInput.java
@@ -1,11 +1,26 @@
 package github.rainbowmori.rainbowapi.object.playerinput;
 
-import org.bukkit.conversations.ConversationFactory;
+import org.bukkit.conversations.*;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
-public class PlayerInput {
+public class PlayerInput implements ConversationAbandonedListener {
+
     private final ConversationFactory factory;
-
-    public PlayerInput(ConversationFactory factory) {
+    public PlayerInput(Plugin plugin, String prefix, boolean cancelable, Prompt prompt) {
+        var factory = new ConversationFactory(plugin).withPrefix(context -> prefix).withFirstPrompt(prompt);
+        if(cancelable) factory.withEscapeSequence("cancel");
+        factory.addConversationAbandonedListener(this).thatExcludesNonPlayersWithMessage("プレイヤーしか入力できません");
         this.factory = factory;
+    }
+
+    public PlayerInput(Plugin plugin, String prefix, boolean cancelable, Prompt prompt,int timeout) {
+        this(plugin, prefix, cancelable, prompt);
+        this.factory.withTimeout(timeout);
+    }
+
+    @Override
+    public void conversationAbandoned(@NotNull ConversationAbandonedEvent abandonedEvent) {
+        if(!abandonedEvent.gracefulExit()) abandonedEvent.getContext().getForWhom().sendRawMessage("入力をキャンセルしました");
     }
 }
