0516d0e507d391443ec0978732496311bcf3ca35
rainbowmori <moriyoshi12345@outlook.jp>
2023-02-03 17:30:18 +0900

    アイテムを渡すアクション

---
+++ src/main/java/github/rainbowmori/rainbowapi/object/ui/action/ClaimAction.java
@@ -0,0 +1,33 @@
+package github.rainbowmori.rainbowapi.object.ui.action;
+
+import github.rainbowmori.rainbowapi.object.ui.gui.MenuHolder;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ClaimAction implements MenuAction{
+    private final ItemStack item;
+    private final boolean copy;
+
+    public ClaimAction(ItemStack item) {
+        this(item, false);
+    }
+
+    public ClaimAction(ItemStack item, boolean copy) {
+        this.item = item;
+        this.copy = copy;
+    }
+
+
+    @Override
+    public void onClick(MenuHolder<?> menuHolder, InventoryClickEvent event) {
+        ItemStack clickedItem = event.getCurrentItem();
+        HumanEntity player = event.getWhoClicked();
+
+        boolean success = clickedItem == null || player.getInventory().addItem(item).isEmpty();
+        if (success && !copy) {
+            event.setCurrentItem(null);
+            menuHolder.unsetButton(event.getSlot());
+        }
+    }
+}
